2325e2324b5419b6195b846d6a630a87ce609b71
Dennis Bölling <derdennis1012@gmail.com>
2025-03-13 15:10:23 +0100

    1

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 # Deinstalliere container-manager, falls bereits installiert
 echo "Deinstalliere container-manager..."
-pip uninstall -y container-manager
+pip3 uninstall -y container-manager
 
 # Installiere oder aktualisiere setuptools und wheel
 echo "Installiere/aktualisiere setuptools und wheel..."
-pip install --upgrade setuptools wheel
+pip3 install --upgrade setuptools wheel
 
 # Erstelle das .whl-Paket
 echo "Erstelle das .whl-Paket..."
-python setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 
 
 # Installiere das gewünschte .whl-Paket
 echo "Installiere das .whl-Paket..."
-pip install dist/container_manager-0.1.0-py3-none-any.whl
+pip3 install dist/container_manager-0.1.0-py3-none-any.whl
 
 echo "Build abgeschlossen!"
